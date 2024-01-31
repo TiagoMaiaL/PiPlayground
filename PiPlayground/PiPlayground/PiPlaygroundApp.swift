316a092053da54e2b8377a9cf0e5584fce3b890f
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2024-01-31 15:01:45 -0300

    Add initial project

---
+++ PiPlayground/PiPlayground/PiPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiPlaygroundApp.swift
+//  PiPlayground
+//
+//  Created by Knowledge Temple on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PiPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
