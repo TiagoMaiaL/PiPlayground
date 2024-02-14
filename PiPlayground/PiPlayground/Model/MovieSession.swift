118070457552d681fc6797ab321aec237d7265ea
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2024-02-13 21:26:21 -0300

    Improve player startup code

--- PiPlayground/PiPlayground/Model/MovieSession.swift
+++ PiPlayground/PiPlayground/Model/MovieSession.swift
@@ -10,7 +10,6 @@ import Combine
 
 final class MovieSession: ObservableObject {
     let movie: Movie
-    
     private var player: AVPlayer?
     
     @Published
@@ -24,25 +23,30 @@ final class MovieSession: ObservableObject {
     func loadVideo() async {
         state = .loading
         
-        debugPrint("Loading player for asset(\(movie.url))")
+        debugPrint("Loading player for asset(\(movie.url)).")
         let player = AVPlayer(url: movie.url)
         
         do {
             guard let isPlayable = try await player.currentItem?.asset.load(.isPlayable), isPlayable else {
-                debugPrint("Asset is not playable :(")
+                debugPrint("Asset is not playable.")
                 state = .failed
                 return
             }
+            self.player = player
             state = .loaded(player: player)
         } catch {
-            debugPrint("Asset couldn't be loaded -> \(error)")
+            debugPrint("Asset couldn't be loaded -> \(error).")
             state = .failed
         }
     }
     
     func startPlayback() {
-        debugPrint("Starting playback")
-        player?.play()
+        if player?.status == .readyToPlay {
+            debugPrint("Starting playback.")
+            player?.play()
+        } else {
+            debugPrint("Couldn't start playback. Player is not ready.")
+        }
     }
 }
 
